d07213eb631840884bb07b3504216a0b1d4d1cfe
Muhammad Haris Mujeeb <harrismuib12@gmail.com>
2026-01-09 19:30:53 +0100

    improved structure

--- bumperbot_controller/bumperbot_controller/simple_controller.py
+++ bumperbot_controller/bumperbot_controller/simple_controller.py
@@ -1,124 +1,170 @@
 #!/usr/bin/env python3
-
 import rclpy
 from rclpy.node import Node
+from rclpy.constants import S_TO_NS
+from rclpy.time import Time
 from std_msgs.msg import Float64MultiArray
-from geometry_msgs.msg import TwistStamped, TransformStamped
+from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import JointState
 from nav_msgs.msg import Odometry
-from rclpy.time import Time
-from rclpy.constants import S_TO_NS
-from tf_transformations import quaternion_from_euler
-from tf2_ros import TransformBroadcaster
 import numpy as np
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
 import math
+from tf_transformations import quaternion_from_euler
+
 
 class SimpleController(Node):
-  def __init__(self):
-    super().__init__(node_name="simple_controller")
 
-    self.declare_parameter("wheel_radius", 0.033)
-    self.declare_parameter("wheel_separation", 0.17)
+    def __init__(self):
+        super().__init__("simple_controller")
+        self.declare_parameter("wheel_radius", 0.033)
+        self.declare_parameter("wheel_separation", 0.17)
 
-    self.wheel_radius_ = self.get_parameter("wheel_radius").get_parameter_value().double_value
-    self.wheel_separation_ = self.get_parameter("wheel_separation").get_parameter_value().double_value
+        self.wheel_radius_ = self.get_parameter("wheel_radius").get_parameter_value().double_value
+        self.wheel_separation_ = self.get_parameter("wheel_separation").get_parameter_value().double_value
 
-    self.get_logger().info(f"Wheel radius: {self.wheel_radius_}")
-    self.get_logger().info(f"Wheel separation: {self.wheel_separation_}")
+        self.get_logger().info("Using wheel radius %d" % self.wheel_radius_)
+        self.get_logger().info("Using wheel separation %d" % self.wheel_separation_)
 
-    self.left_wheel_prev_pos_ = 0.0
-    self.right_wheel_prev_pos_ = 0.0
-    self.prev_time_ = self.get_clock().now()
+        self.left_wheel_prev_pos_ = 0.0
+        self.right_wheel_prev_pos_ = 0.0
 
-    self.x_ = 0.0
-    self.y_ = 0.0
-    self.theta_ = 0.0
+        self.prev_pose_init_ = False
+        self.x_ = 0.0
+        self.y_ = 0.0
+        self.theta_ = 0.0
 
-    self.wheel_cmd_pub = self.create_publisher(Float64MultiArray, "simple_velocity_controller/commands", 10)
-    self.vel_sub = self.create_subscription(TwistStamped, "/bumperbot_controller/cmd_vel", self.vel_callback, 10)
+        self.wheel_cmd_pub_ = self.create_publisher(Float64MultiArray, "simple_velocity_controller/commands", 10)
+        self.vel_sub_ = self.create_subscription(TwistStamped, "bumperbot_controller/cmd_vel", self.velCallback, 10)
+        self.joint_sub_ = self.create_subscription(JointState,"joint_states", self.jointCallback, 10)        
+        self.odom_pub_ = self.create_publisher(Odometry, "bumperbot_controller/odom", 10)
 
-    self.speed_conversion_ = np.array([[self.wheel_radius_/2, self.wheel_radius_/2],
-                                        [self.wheel_radius_/self.wheel_separation_, -self.wheel_radius_/self.wheel_separation_]])
-    self.joint_sub_ = self.create_subscription(JointState, "joint_states", self.joint_callback, 10)
-    self.odom_pub_ = self.create_publisher(Odometry, "bumperbot_controller/odom", 10)
+        self.speed_conversion_ = np.array([[self.wheel_radius_/2, self.wheel_radius_/2],
+                                           [self.wheel_radius_/self.wheel_separation_, -self.wheel_radius_/self.wheel_separation_]])
+        self.get_logger().info("The conversion matrix is %s" % self.speed_conversion_)
 
-    self.odom_msg_ = Odometry()
-    self.odom_msg_.header.frame_id = "bumperbot_controller/odom"
-    self.odom_msg_.child_frame_id = "base_footprint"
-    self.odom_msg_.pose.pose.orientation.w = 0.0
-    self.odom_msg_.pose.pose.orientation.x = 0.0
-    self.odom_msg_.pose.pose.orientation.y = 0.0
-    self.odom_msg_.pose.pose.orientation.z = 0.0
-    self.odom_msg_.pose.pose.position.x = 0.0
-    self.odom_msg_.pose.pose.position.y = 0.0
-    self.odom_msg_.pose.pose.position.z = 0.0
-    self.odom_msg_.twist.twist.linear.x = 0.0
-    self.odom_msg_.twist.twist.angular.z = 0.0
+        # Fill the Odometry message with invariant parameters
+        self.odom_msg_ = Odometry()
+        self.odom_msg_.header.frame_id = "odom"
+        self.odom_msg_.child_frame_id = "base_footprint"
+        self.odom_msg_.pose.pose.orientation.x = 0.0
+        self.odom_msg_.pose.pose.orientation.y = 0.0
+        self.odom_msg_.pose.pose.orientation.z = 0.0
+        self.odom_msg_.pose.pose.orientation.w = 1.0
 
-    self.br_ = TransformBroadcaster(self)
-    self.transform_stamped_ = TransformStamped()
-    self.transform_stamped_.header.frame_id = "bumperbot_controller/odom"
-    self.transform_stamped_.child_frame_id = "base_footprint"
+        # Fill the TF message
+        self.br_ = TransformBroadcaster(self)
+        self.transform_stamped_ = TransformStamped()
+        self.transform_stamped_.header.frame_id = "odom"
+        self.transform_stamped_.child_frame_id = "base_footprint"
 
-
-  def vel_callback(self, msg):
-    robot_speed = np.array([msg.twist.linear.x, 
-                            msg.twist.angular.z])
-    wheel_speed = np.matmul(np.linalg.inv(self.speed_conversion_), robot_speed)
-
-    wheel_speed_msg = Float64MultiArray()
-    wheel_speed_msg.data = [wheel_speed[0], wheel_speed[1]]
-    self.wheel_cmd_pub.publish(wheel_speed_msg)
+        self.prev_time_ = self.get_clock().now()
 
 
-  def joint_callback(self, msg):
-    dp_left = msg.position[1] - self.left_wheel_prev_pos_
-    dp_right = msg.position[0] - self.right_wheel_prev_pos_
-    dt = Time.from_msg(msg.header.stamp) - self.prev_time_
+    def velCallback(self, msg):
+        # Implements the differential kinematic model
+        # Given v and w, calculate the velocities of the wheels
+        robot_speed = np.array([[msg.twist.linear.x],
+                                [msg.twist.angular.z]])
+        wheel_speed = np.matmul(np.linalg.inv(self.speed_conversion_), robot_speed) 
 
-    self.left_wheel_prev_pos_ = msg.position[1]
-    self.right_wheel_prev_pos_ = msg.position[0]
-    self.prev_time_ = Time.from_msg(msg.header.stamp)
+        wheel_speed_msg = Float64MultiArray()
+        wheel_speed_msg.data = [wheel_speed[1, 0], wheel_speed[0, 0]]
 
-    fi_left = dp_left/ (dt.nanoseconds / S_TO_NS)
-    fi_right = dp_right/ (dt.nanoseconds / S_TO_NS)
+        self.wheel_cmd_pub_.publish(wheel_speed_msg)
 
-    linear = (self.wheel_radius_ * fi_right + self.wheel_radius_ * fi_left) / 2
-    angular = (self.wheel_radius_ * fi_right - self.wheel_radius_ * fi_left) / self.wheel_separation_
+    
+    def jointCallback(self, msg: JointState):
+        # Implements the inverse differential kinematic model
+        # Given the position of the wheels, calculates their velocities
+        # then calculates the velocity of the robot wrt the robot frame
+        # and then converts it in the global frame and publishes the TF
 
-    d_s = (self.wheel_radius_ * dp_right + self.wheel_radius_ * dp_left) / 2
-    d_theta = (self.wheel_radius_ * dp_right - self.wheel_radius_ * dp_left) / self.wheel_separation_
-    self.theta_ += d_theta
-    self.x_ += d_s * math.cos(self.theta_)
-    self.y_ += d_s * math.sin(self.theta_)
+        if not self.prev_pose_init_:
+          self.left_wheel_prev_pos_ = msg.position[0]
+          self.right_wheel_prev_pos_ = msg.position[1]
+          self.prev_time_ = Time.from_msg(msg.header.stamp)
+          self.prev_pose_init_ = True
+          return
 
-    q = quaternion_from_euler(0, 0, self.theta_)
-    self.odom_msg_.pose.pose.orientation.w = q[3]
-    self.odom_msg_.pose.pose.orientation.x = q[0]
-    self.odom_msg_.pose.pose.orientation.y = q[1]
-    self.odom_msg_.pose.pose.orientation.z = q[2]
-    self.odom_msg_.header.stamp = self.get_clock().now().to_msg()
-    self.odom_msg_.pose.pose.position.x = self.x_
-    self.odom_msg_.pose.pose.position.y = self.y_
-    self.odom_msg_.twist.twist.linear.x = linear
-    self.odom_msg_.twist.twist.angular.z = angular
+        dp_left = msg.position[0] - self.left_wheel_prev_pos_
+        dp_right = msg.position[1] - self.right_wheel_prev_pos_
+        dt = Time.from_msg(msg.header.stamp) - self.prev_time_
 
-    self.transform_stamped_.transform.rotation.w = q[3]
-    self.transform_stamped_.transform.rotation.x = q[0]
-    self.transform_stamped_.transform.rotation.y = q[1]
-    self.transform_stamped_.transform.rotation.z = q[2]
-    self.transform_stamped_.header.stamp = self.get_clock().now().to_msg()
-    self.transform_stamped_.transform.translation.x = self.x_
-    self.transform_stamped_.transform.translation.y = self.y_
+        # Actualize the prev pose for the next itheration
+        self.left_wheel_prev_pos_ = msg.position[0]
+        self.right_wheel_prev_pos_ = msg.position[1]
+        self.prev_time_ = Time.from_msg(msg.header.stamp)
 
-    self.odom_pub_.publish(self.odom_msg_)
-    self.br_.sendTransform(self.transform_stamped_)
+        if dt.nanoseconds == 0:
+          return # Avoid crashing
+        
+        # Calculate the rotational speed of each wheel
+        fi_left = dp_left / (dt.nanoseconds / S_TO_NS)
+        fi_right = dp_right / (dt.nanoseconds / S_TO_NS)
 
-def main(args=None):
-  rclpy.init(args=args)
-  simple_controller = SimpleController()
-  rclpy.spin(simple_controller)
-  rclpy.shutdown()
+        # Calculate the linear and angular velocity
+        linear = (self.wheel_radius_ * fi_right + self.wheel_radius_ * fi_left) / 2
+        angular = (self.wheel_radius_ * fi_right - self.wheel_radius_ * fi_left) / self.wheel_separation_
 
-if __name__ == "__main__":
-  main()
+        # Calculate the position increment
+        d_s = (self.wheel_radius_ * dp_right + self.wheel_radius_ * dp_left) / 2
+        d_theta = (self.wheel_radius_ * dp_right - self.wheel_radius_ * dp_left) / self.wheel_separation_
+        self.theta_ += d_theta
+
+        # Normalize theta to be between -pi and pi
+        self.theta_ = math.atan2(math.sin(self.theta_), math.cos(self.theta_))
+        
+        
+        self.x_ += d_s * math.cos(self.theta_)
+        self.y_ += d_s * math.sin(self.theta_)
+        
+# --- DEBUG BLOCK (Insert before self.odom_pub_.publish) ---
+        # Only print every 20th message to avoi dspamming the terminal
+        if msg.header.stamp.nanosec % 200 == 0: 
+            self.get_logger().debug(
+                f"\n"
+                f"--- ODOM DEBUG ---\n"
+                f"Deltas  -> Left: {dp_left:.4f}  Right: {dp_right:.4f}\n"
+                f"Increm  -> d_s:  {d_s:.4f}    d_th:  {d_theta:.4f}\n"
+                f"Pose    -> X:    {self.x_:.4f}    Y:     {self.y_:.4f}    Th: {self.theta_:.4f}\n"
+                f"------------------"
+            )
+
+        # Compose and publish the odom message
+        q = quaternion_from_euler(0, 0, self.theta_)
+        self.odom_msg_.header.stamp = self.get_clock().now().to_msg()
+        self.odom_msg_.pose.pose.position.x = self.x_
+        self.odom_msg_.pose.pose.position.y = self.y_
+        self.odom_msg_.pose.pose.orientation.x = q[0]
+        self.odom_msg_.pose.pose.orientation.y = q[1]
+        self.odom_msg_.pose.pose.orientation.z = q[2]
+        self.odom_msg_.pose.pose.orientation.w = q[3]
+        self.odom_msg_.twist.twist.linear.x = linear
+        self.odom_msg_.twist.twist.angular.z = angular
+        self.odom_pub_.publish(self.odom_msg_)
+
+        # TF
+        self.transform_stamped_.transform.translation.x = self.x_
+        self.transform_stamped_.transform.translation.y = self.y_
+        self.transform_stamped_.transform.rotation.x = q[0]
+        self.transform_stamped_.transform.rotation.y = q[1]
+        self.transform_stamped_.transform.rotation.z = q[2]
+        self.transform_stamped_.transform.rotation.w = q[3]
+        self.transform_stamped_.header.stamp = self.get_clock().now().to_msg()
+        self.br_.sendTransform(self.transform_stamped_)
+
+
+def main():
+    rclpy.init()
+
+    simple_controller = SimpleController()
+    rclpy.spin(simple_controller)
+    
+    simple_controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
